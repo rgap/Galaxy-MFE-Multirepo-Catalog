850800338da72bb504c3d0f427fd9968b80d5497
rgap <r.guzmanap@gmail.com>
2025-06-10 00:07:44 -0500

    fix: remove bootstrap.js
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    src/bootstrap.js
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import('./bootstrap'); 
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+// Mount function for running the app both standalone and as a remote
+const mount = element => {
+  const root = createRoot(element);
+
+  root.render(
+    <React.StrictMode>
+      {/* Wrap with BrowserRouter only in development/standalone mode */}
+      {process.env.NODE_ENV === "development" ? (
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      ) : (
+        <App />
+      )}
+    </React.StrictMode>
+  );
+
+  return root;
+};
+
+// If we are in development and running the app standalone
+if (process.env.NODE_ENV === "development") {
+  const element = document.getElementById("root");
+  if (element) {
+    mount(element);
+  }
+}
+
+// Export the mount function for container usage
+export { mount };
