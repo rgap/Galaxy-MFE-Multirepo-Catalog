65c71bd6858d678cebb010711104ec1b397b4976
rgap <r.guzmanap@gmail.com>
2025-06-10 02:33:46 -0500

    feat: reorganize folder structure
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	deleted:    src/hooks/index.js
    	modified:   src/index.js
    	deleted:    src/utils/index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import CatalogPage from "./pages/CatalogPage";
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
 // Mount function for running the app both standalone and as a remote
 const mount = element => {
